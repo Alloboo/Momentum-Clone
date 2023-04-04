a8eaa614691c0daf113f7e7d4ccf81f0d94aeca9
Alloboo <hye_0425@naver.com>
2023-04-05 02:25:26 +0900

    0404

---
+++ momentum/js/quotes.js
@@ -0,0 +1,22 @@
+const quotes = [
+    {
+        quote: `They don't make bugs like Bunny anymore.`,
+        author: `Olav Mjelde.`
+    },
+    {
+        quote: `Don't worry if it doesn't work right. If everything did, you'd be out of a job.`,
+        author: `Eric S. Raymond`
+    },
+    {
+        quote: `Most of you are familiar with the virtues of a programmer. There are three, of course: laziness, impatience, and hubris.`,
+        author: `Larry Wall`,
+    }
+]
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
