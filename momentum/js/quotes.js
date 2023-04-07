0c8ccbcd77c3d39a274ce2dab93cc6166af3be99
Alloboo <hye_0425@naver.com>
2023-04-07 23:55:43 +0900

    modified

--- momentum/js/quotes.js
+++ momentum/js/quotes.js
@@ -15,10 +15,10 @@ const quotes = [
     }
 ]
 
-const quote = document.querySelector("#quote span:first-child");
-const author = document.querySelector("#quote span:last-child");
+const quoteElem = document.querySelector("#quote span:first-child");
+const authorElem = document.querySelector("#quote span:last-child");
 
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
-quote.innerText = todaysQuote.quote;
-author.innerText = todaysQuote.author;
+quoteElem.innerText = todaysQuote.quote;
+authorElem.innerText = todaysQuote.author;
